a9693031eafce78fc276fa133e9447fd3a337e52
matt-hammond-bbc <matt.hammond@bbc.co.uk>
2016-11-17 20:46:59 +0000

    Beginnings of doc build process

---
+++ build-docs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+COMMON="jazzy.common.yaml"
+
+for CONFIG_TEMPLATE in `find . -name jazzy.template.yaml`; do
+	SUBDIR=`dirname "$CONFIG_TEMPLATE"`
+	cat "$CONFIG_TEMPLATE" "$COMMON" > "$SUBDIR/jazzy.merged.yaml"
+	(
+		cd $SUBDIR;
+		jazzy --config jazzy.merged.yaml;
+	)
+done

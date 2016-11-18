afc78eabcd84bd8ce28ce721df266ab869b6e532
matt-hammond-bbc <matt.hammond@bbc.co.uk>
2016-11-18 11:36:21 +0000

    Moved doc building into a subdir

--- docs/make-docs.sh
+++ docs/make-docs.sh
@@ -2,7 +2,7 @@
 
 COMMON="jazzy.common.yaml"
 
-for CONFIG_TEMPLATE in `find . -name jazzy.template.yaml`; do
+for CONFIG_TEMPLATE in `find .. -name jazzy.template.yaml`; do
 	SUBDIR=`dirname "$CONFIG_TEMPLATE"`
 	cat "$CONFIG_TEMPLATE" "$COMMON" > "$SUBDIR/jazzy.merged.yaml"
 	(
